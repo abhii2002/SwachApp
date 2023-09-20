18e0d4750cc7006c9fa2aadcab2a132961cc0392
Manvi-Chaudhary <manvichaudhary2003@gmail.com>
2023-09-20 15:12:31 +0530

    Scan Completed

--- app/src/main/java/com/blissvine/swach/MainActivity.kt
+++ app/src/main/java/com/blissvine/swach/MainActivity.kt
@@ -1,18 +1,45 @@
 package com.blissvine.swach
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import com.blissvine.swach.databinding.ActivityMainBinding
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+
+    lateinit var imageUri : Uri
+
+    val contracts =registerForActivityResult(ActivityResultContracts.TakePicture()){
+        Log.d("image",imageUri.toString())
+        if(imageUri!=null){
+            val intent= Intent(applicationContext,ScanActivity::class.java)
+            intent.putExtra("image",imageUri.toString())
+            startActivity(intent)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
          binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         val view = binding.root
         setContentView(view)
+        imageUri=createImageUri()!!
+        binding.scanBtn.setOnClickListener {
+            contracts.launch(imageUri)
+        }
 
+    }
+
+    fun createImageUri() : Uri? {
+        val image= File(applicationContext.filesDir,"camera_photo.png")
+        return FileProvider.getUriForFile(applicationContext,"com.blissvine.swach.fileProvider",image)
     }
 }
---
+++ app/src/main/java/com/blissvine/swach/ScanActivity.kt
@@ -0,0 +1,30 @@
+package com.blissvine.swach
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toUri
+import com.blissvine.swach.databinding.ActivityMainBinding
+
+class ScanActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+        val img=intent.getStringExtra("image")
+        val imageView : ImageView =findViewById(R.id.imageViewscan)
+
+        if(img!=null){
+            Log.d("intent",img)
+            imageView.setImageURI(img.toUri())
+        }
+        val btn : Button = findViewById(R.id.submitBtn)
+        btn.setOnClickListener {
+            Toast.makeText(this,"Button clicked",Toast.LENGTH_SHORT).show()
+        }
+    }
+}
