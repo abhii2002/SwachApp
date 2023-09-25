aa4f91c8cd560c428482605883b1637c39e9f88b
Manvi-Chaudhary <manvichaudhary2003@gmail.com>
2023-09-25 11:53:45 +0530

    Daily waste activity

---
+++ app/src/main/java/com/blissvine/swach/activities/DailyWasteActivity.kt
@@ -0,0 +1,12 @@
+package com.blissvine.swach.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.blissvine.swach.R
+
+class DailyWasteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dailywaste)
+    }
+}
