034bdc1c78d0a44113da55b179ea1f10f9d0b15a
abhii2002 <as0709716@gmail.com>
2023-09-26 21:08:41 +0530

    updated

---
+++ app/src/main/java/com/blissvine/swach/activities/VendorsDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.blissvine.swach.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blissvine.swach.R
+
+class VendorsDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendors_details)
+    }
+}
---
+++ app/src/main/res/drawable/VendorDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.blissvine.swach.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.blissvine.swach.R
+
+class VendorDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_vendor_details)
+    }
+}
